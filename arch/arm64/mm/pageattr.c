64303beaf0cb821ab78ee4409480a734b6eb93c9
jimsterino98 <n.n.hoxha@gmail.com>
2026-03-22 00:10:33 -0700

    Update pageattr.c
    TRYING TO GET IT TO WORK AGAIN

--- arch/arm64/mm/pageattr.c
+++ arch/arm64/mm/pageattr.c
@@ -55,6 +55,47 @@ static int __change_memory_common(unsigned long start, unsigned long size,
 	return ret;
 }
 
+#ifdef KPM_BUILD
+static int change_memory_common(unsigned long addr, int numpages,
+				pgprot_t set_mask, pgprot_t clear_mask)
+{
+	unsigned long start = addr;
+	unsigned long size = PAGE_SIZE * numpages;
+	unsigned long end = start + size;
+	struct vm_struct *area;
+	int i;
+
+	if (!PAGE_ALIGNED(addr)) {
+		start &= PAGE_MASK;
+		end = start + size;
+		WARN_ON_ONCE(1);
+	}
+
+	area = find_vm_area((void *)addr);
+	if (area) {
+		if (end > (unsigned long)kasan_reset_tag(area->addr) + area->size ||
+		    !(area->flags & VM_ALLOC))
+			return -EINVAL;
+	}
+
+	if (!numpages)
+		return 0;
+
+	if (rodata_full && (pgprot_val(set_mask) == PTE_RDONLY ||
+			    pgprot_val(clear_mask) == PTE_RDONLY)) {
+		if (area) {
+			for (i = 0; i < area->nr_pages; i++) {
+				__change_memory_common((u64)page_address(area->pages[i]),
+						       PAGE_SIZE, set_mask, clear_mask);
+			}
+		}
+	}
+
+	vm_unmap_aliases();
+
+	return __change_memory_common(start, size, set_mask, clear_mask);
+}
+#else
 static int change_memory_common(unsigned long addr, int numpages,
 				pgprot_t set_mask, pgprot_t clear_mask)
 {
@@ -112,6 +153,7 @@ static int change_memory_common(unsigned long addr, int numpages,
 
 	return __change_memory_common(start, size, set_mask, clear_mask);
 }
+#endif
 
 int set_memory_ro(unsigned long addr, int numpages)
 {
