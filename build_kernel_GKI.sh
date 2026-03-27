986aa96ec33f65f50491e2d853e715cb95c551de
jimsterino98 <n.n.hoxha@gmail.com>
2026-03-27 12:02:11 -0700

    Update build_kernel_GKI.sh
    added build number

--- build_kernel_GKI.sh
+++ build_kernel_GKI.sh
@@ -7,6 +7,7 @@ export PROJECT_NAME=${MODEL}
 export REGION=$(echo $BUILD_TARGET | cut -d'_' -f2)
 export CARRIER=$(echo $BUILD_TARGET | cut -d'_' -f3)
 export TARGET_BUILD_VARIANT= user
+export BUILD_NUMBER=S928BXXS4AYB6
 		
 		
 #2. sm8650 common config
