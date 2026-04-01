6d1a322e1f7ab95d628149b0e6abdd84c6c69f47
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-01 10:37:07 -0700

    Refactor includes in base.c
    added dma-buf include and moved others for susfs patching

--- kernel_platform/common/fs/proc/base.c
+++ kernel_platform/common/fs/proc/base.c
@@ -85,6 +85,8 @@
 #include <linux/elf.h>
 #include <linux/pid_namespace.h>
 #include <linux/user_namespace.h>
+#include <linux/task_integrity.h>
+#include <linux/proca.h>
 #include <linux/fs_parser.h>
 #include <linux/fs_struct.h>
 #include <linux/slab.h>
@@ -99,8 +101,7 @@
 #include <linux/cn_proc.h>
 #include <linux/ksm.h>
 #include <linux/cpufreq_times.h>
-#include <linux/task_integrity.h>
-#include <linux/proca.h>
+#include <linux/dma-buf.h>
 #include <trace/events/oom.h>
 #include <trace/hooks/sched.h>
 #include "internal.h"
