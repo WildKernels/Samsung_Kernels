17c640bd544a607845589033defd06dc9ee4cf60
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-01 10:02:54 -0700

    moved includes in base.c for susfs

--- kernel_platform/common/fs/proc/base.c
+++ kernel_platform/common/fs/proc/base.c
@@ -85,6 +85,9 @@
 #include <linux/elf.h>
 #include <linux/pid_namespace.h>
 #include <linux/user_namespace.h>
+#include <trace/hooks/sched.h>
+#include <linux/task_integrity.h>
+#include <linux/proca.h>
 #include <linux/fs_struct.h>
 #include <linux/slab.h>
 #include <linux/sched/autogroup.h>
@@ -97,10 +100,7 @@
 #include <linux/resctrl.h>
 #include <linux/cn_proc.h>
 #include <linux/cpufreq_times.h>
-#include <linux/task_integrity.h>
-#include <linux/proca.h>
 #include <trace/events/oom.h>
-#include <trace/hooks/sched.h>
 #include "internal.h"
 #include "fd.h"
 
