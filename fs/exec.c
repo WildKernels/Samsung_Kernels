1c20792b6198393bbf008921954f7744ae5a7c33
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-01 21:27:52 -0700

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -53,6 +53,7 @@
 #include <linux/mount.h>
 #include <linux/security.h>
 #include <linux/syscalls.h>
+#include <linux/task_integrity.h>
 #include <linux/tsacct_kern.h>
 #include <linux/cn_proc.h>
 #include <linux/audit.h>
@@ -65,7 +66,6 @@
 #include <linux/vmalloc.h>
 #include <linux/io_uring.h>
 #include <linux/syscall_user_dispatch.h>
-#include <linux/task_integrity.h>
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
