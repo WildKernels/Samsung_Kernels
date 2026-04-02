0d3d945f4b77af12b8ad79ce64a1b9ca406e9235
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-01 21:20:53 -0700

    Update base.c

--- kernel/fs/proc/base.c
+++ kernel/fs/proc/base.c
@@ -86,6 +86,8 @@
 #include <linux/elf.h>
 #include <linux/pid_namespace.h>
 #include <linux/user_namespace.h>
+#include <linux/task_integrity.h>
+#include <linux/proca.h>
 #include <linux/fs_parser.h>
 #include <linux/fs_struct.h>
 #include <linux/slab.h>
@@ -99,8 +101,7 @@
 #include <linux/resctrl.h>
 #include <linux/cn_proc.h>
 #include <linux/cpufreq_times.h>
-#include <linux/task_integrity.h>
-#include <linux/proca.h>
+#include <linux/dma-buf.h>
 #include <trace/events/oom.h>
 #include <trace/hooks/sched.h>
 #include "internal.h"
