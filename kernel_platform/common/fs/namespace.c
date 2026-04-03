ba860bf45c12e8a0726e04ed76e77b024f74bee6
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-03 08:00:18 -0700

    Update namespace.c

--- kernel_platform/common/fs/namespace.c
+++ kernel_platform/common/fs/namespace.c
@@ -22,6 +22,10 @@
 #include <linux/fsnotify.h>	/* fsnotify_vfsmount_delete */
 #include <linux/file.h>
 #include <linux/uaccess.h>
+#include <linux/fslog.h>
+#ifdef CONFIG_KDP_NS
+#include <linux/kdp.h>
+#endif
 #include <linux/proc_ns.h>
 #include <linux/magic.h>
 #include <linux/memblock.h>
@@ -32,10 +36,6 @@
 #include <linux/fs_context.h>
 #include <linux/shmem_fs.h>
 #include <linux/mnt_idmapping.h>
-#include <linux/fslog.h>
-#ifdef CONFIG_KDP_NS
-#include <linux/kdp.h>
-#endif
 
 #include "pnode.h"
 #include "internal.h"
