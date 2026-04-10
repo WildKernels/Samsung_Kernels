beffed2cca078836c28291b9b088db83cf72355d
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-10 16:22:51 -0700

    Update build_kernel_GKI.sh with new target settings

--- build_kernel_GKI.sh
+++ build_kernel_GKI.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 #1. target config
-BUILD_TARGET=$1
+BUILD_TARGET=e3q_eur_openx
 export MODEL=$(echo $BUILD_TARGET | cut -d'_' -f1)
 export PROJECT_NAME=${MODEL}
 export REGION=$(echo $BUILD_TARGET | cut -d'_' -f2)
 export CARRIER=$(echo $BUILD_TARGET | cut -d'_' -f3)
-export TARGET_BUILD_VARIANT=$2
+export TARGET_BUILD_VARIANT= user
+export BUILD_NUMBER=S928BXXS5CZB2
 		
 		
 #2. sm8650 common config
-CHIPSET_NAME=$3
+CHIPSET_NAME=pineapple
 
 export ANDROID_BUILD_TOP=$(pwd)
 export TARGET_PRODUCT=gki
@@ -34,4 +35,4 @@ export KBUILD_EXT_MODULES="\
 
 #3. build kernel
 cd ./kernel_platform/
-RECOMPILE_KERNEL=1 ./build/android/prepare_vendor.sh ${CHIPSET_NAME} ${TARGET_PRODUCT} gki | tee -a ../build.log
+RECOMPILE_KERNEL=1 ./build/android/prepare_vendor.sh ${CHIPSET_NAME} ${TARGET_PRODUCT} sec | tee -a ../build.log
