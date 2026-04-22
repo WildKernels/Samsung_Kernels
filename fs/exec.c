e948d41b441c684dcc0e27179a9aaa883899f947
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-21 19:47:18 -0700

    moved include of task_integrity.h

--- fs/exec.c
+++ fs/exec.c
@@ -43,6 +43,7 @@
 #include <linux/perf_event.h>
 #include <linux/highmem.h>
 #include <linux/spinlock.h>
+#include <linux/task_integrity.h>
 #include <linux/key.h>
 #include <linux/personality.h>
 #include <linux/binfmts.h>
@@ -65,7 +66,6 @@
 #include <linux/vmalloc.h>
 #include <linux/io_uring.h>
 #include <linux/syscall_user_dispatch.h>
-#include <linux/task_integrity.h>
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
