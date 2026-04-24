f5a303b1bfcfe2f8d544392a62c93bb8e9272b95
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-23 21:52:52 -0700

    Add fslog.h include to namespace.c

--- kernel-5.10/fs/namespace.c
+++ kernel-5.10/fs/namespace.c
@@ -15,6 +15,7 @@
 #include <linux/user_namespace.h>
 #include <linux/namei.h>
 #include <linux/security.h>
+#include <linux/fslog.h>
 #include <linux/cred.h>
 #include <linux/idr.h>
 #include <linux/init.h>		/* init_rootfs */
@@ -30,7 +31,6 @@
 #include <uapi/linux/mount.h>
 #include <linux/fs_context.h>
 #include <linux/shmem_fs.h>
-#include <linux/fslog.h>
 
 #include "pnode.h"
 #include "internal.h"
