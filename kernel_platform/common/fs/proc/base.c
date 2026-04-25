b1062a4983664597d1342b16bd7d5d6b31bf5251
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-25 04:16:05 -0700

    Refactor includes in base.c for clarity

--- kernel_platform/common/fs/proc/base.c
+++ kernel_platform/common/fs/proc/base.c
@@ -71,6 +71,8 @@
 #include <linux/stacktrace.h>
 #include <linux/resource.h>
 #include <linux/module.h>
+#include <linux/task_integrity.h>
+#include <linux/proca.h>
 #include <linux/mount.h>
 #include <linux/security.h>
 #include <linux/ptrace.h>
@@ -98,9 +100,6 @@
 #include <linux/resctrl.h>
 #include <linux/cn_proc.h>
 #include <linux/cpufreq_times.h>
-#include <linux/cn_proc.h>
-#include <linux/task_integrity.h>
-#include <linux/proca.h>
 #include <trace/events/oom.h>
 #include "internal.h"
 #include "fd.h"
