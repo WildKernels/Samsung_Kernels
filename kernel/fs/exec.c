884590ed7e0b34c6e7c9a66857211c16fc919f36
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-24 18:20:25 -0700

    move include of task_integrity.h

--- kernel/fs/exec.c
+++ kernel/fs/exec.c
@@ -42,6 +42,7 @@
 #include <linux/perf_event.h>
 #include <linux/highmem.h>
 #include <linux/spinlock.h>
+#include <linux/task_integrity.h>
 #include <linux/key.h>
 #include <linux/personality.h>
 #include <linux/binfmts.h>
@@ -63,7 +64,6 @@
 #include <linux/vmalloc.h>
 #include <linux/io_uring.h>
 #include <linux/syscall_user_dispatch.h>
-#include <linux/task_integrity.h>
 #include <linux/coredump.h>
 
 #include <linux/uaccess.h>
