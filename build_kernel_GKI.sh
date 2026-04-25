510c28fc91bf1ea41f3fdd1bbb6498c4d8f8e206
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-25 03:51:39 -0700

    Add BUILD_NUMBER variable to build script

--- build_kernel_GKI.sh
+++ build_kernel_GKI.sh
@@ -7,6 +7,7 @@ export PROJECT_NAME=${MODEL}
 export REGION=$(echo $BUILD_TARGET | cut -d'_' -f2)
 export CARRIER=$(echo $BUILD_TARGET | cut -d'_' -f3)
 export TARGET_BUILD_VARIANT=user
+export BUILD_NUMER=X710XXU5BXKD
 		
 		
 #2. sm8550 common config
