1a65fb71a8f823e46a2b05120d612f9b6e946db9
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-25 03:39:57 -0700

    move includes in namespace.c

--- kernel_platform/common/fs/namespace.c
+++ kernel_platform/common/fs/namespace.c
@@ -15,6 +15,10 @@
 #include <linux/user_namespace.h>
 #include <linux/namei.h>
 #include <linux/security.h>
+#include <linux/fslog.h>
+#ifdef CONFIG_KDP_NS
+#include <linux/kdp.h>
+#endif
 #include <linux/cred.h>
 #include <linux/idr.h>
 #include <linux/init.h>		/* init_rootfs */
@@ -32,10 +36,6 @@
 #include <linux/fs_context.h>
 #include <linux/shmem_fs.h>
 #include <linux/mnt_idmapping.h>
-#include <linux/fslog.h>
-#ifdef CONFIG_KDP_NS
-#include <linux/kdp.h>
-#endif
 
 #include "pnode.h"
 #include "internal.h"
