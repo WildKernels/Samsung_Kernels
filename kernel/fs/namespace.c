09bd94c245396914e9cc6a97b7d26996d18e4c61
jimsterino98 <n.n.hoxha@gmail.com>
2026-04-25 08:46:21 -0700

    Update namespace.c

--- kernel/fs/namespace.c
+++ kernel/fs/namespace.c
@@ -15,6 +15,7 @@
 #include <linux/user_namespace.h>
 #include <linux/namei.h>
 #include <linux/security.h>
+#include <linux/delay.h>
 #include <linux/fslog.h>
 #include <linux/cred.h>
 #include <linux/idr.h>
@@ -33,7 +34,6 @@
 #include <linux/fs_context.h>
 #include <linux/shmem_fs.h>
 #include <linux/mnt_idmapping.h>
-#include <linux/delay.h>
 
 #include "pnode.h"
 #include "internal.h"
