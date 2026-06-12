fc39f0257b72317dd9806b3fe70318a03e826ce4
jimsterino98 <n.n.hoxha@gmail.com>
2026-06-11 19:24:13 -0700

    moved task_integrity.h include

--- kernel_platform/common/fs/exec.c
+++ kernel_platform/common/fs/exec.c
@@ -45,6 +45,7 @@
 #include <linux/spinlock.h>
 #include <linux/key.h>
 #include <linux/personality.h>
+#include <linux/task_integrity.h>
 #include <linux/binfmts.h>
 #include <linux/utsname.h>
 #include <linux/pid_namespace.h>
@@ -65,7 +66,6 @@
 #include <linux/vmalloc.h>
 #include <linux/io_uring.h>
 #include <linux/syscall_user_dispatch.h>
-#include <linux/task_integrity.h>
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
