cfcb2d43e5f6eba5a16945dddddb528ecc0e953c
jimsterino98 <n.n.hoxha@gmail.com>
2026-06-19 00:47:02 -0700

    Add build_kernel.sh script for fast kernel building

---
+++ build_kernel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set QUICK_BUILD=1 to only build the kernel image (skip modules, dist, GKI checker)
+export QUICK_BUILD=1
+
+cd kernel
+python kernel_device_modules-6.6/scripts/gen_build_config.py --kernel-defconfig mediatek-bazel_defconfig --kernel-defconfig-overlays "mt6768_overlay.config S96818AA1.config S96818AA1_debug.config" --kernel-build-config-overlays "" -m user -o ../out/target/product/a05m/obj/KERNEL_OBJ/build.config
+
+export DEVICE_MODULES_DIR="kernel_device_modules-6.6"
+export BUILD_CONFIG="../out/target/product/a05m/obj/KERNEL_OBJ/build.config"
+export OUT_DIR="../out/target/product/a05m/obj/KLEAF_OBJ"
+export DIST_DIR="../out/target/product/a05m/obj/KLEAF_OBJ/dist"
+export DEFCONFIG_OVERLAYS="mt6768_overlay.config S96818AA1.config S96818AA1_debug.config"
+export PROJECT="mgk_64_k66"
+export MODE="user"
+export BUILD_NUMBER=A055FXXUEDZB2
+
+./kernel_device_modules-6.6/build.sh
