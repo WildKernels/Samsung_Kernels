bcdae11bc298101389f0d8ac43c77e8d2929542b
jimsterino98 <n.n.hoxha@gmail.com>
2026-06-20 00:10:47 -0700

    Include kernel-only argument in prepare_vendor.sh
    Added kernel-only argument to build script.

--- kernel_platform/build/kernel/android/prepare_vendor.sh
+++ kernel_platform/build/kernel/android/prepare_vendor.sh
@@ -263,7 +263,7 @@ if [ "${RECOMPILE_KERNEL}" == "1" ]; then
   fi
   "${ROOT_DIR}/build_with_bazel.py" \
     -t "$KERNEL_TARGET" "$KERNEL_VARIANT" $LTO_KBUILD_ARG $EXTRA_KBUILD_ARGS --skip abl \
-    --out_dir "${ANDROID_KP_OUT_DIR}"
+    --out_dir "${ANDROID_KP_OUT_DIR}" $KERNEL_ONLY_ARG
 
   COPY_NEEDED=1
 fi
