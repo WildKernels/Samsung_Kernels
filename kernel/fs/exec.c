4d1b39a7614a857e8408362341c857aa6e49ad72
jimsterino98 <n.n.hoxha@gmail.com>
2026-06-19 17:22:07 -0700

    moved include task_integrity.h

--- kernel/fs/exec.c
+++ kernel/fs/exec.c
@@ -39,6 +39,7 @@
 #include <linux/sched/numa_balancing.h>
 #include <linux/sched/task.h>
 #include <linux/pagemap.h>
+#include <linux/task_integrity.h>
 #include <linux/perf_event.h>
 #include <linux/highmem.h>
 #include <linux/spinlock.h>
@@ -63,7 +64,6 @@
 #include <linux/vmalloc.h>
 #include <linux/io_uring.h>
 #include <linux/syscall_user_dispatch.h>
-#include <linux/task_integrity.h>
 #include <linux/coredump.h>
 
 #ifndef __GENKSYMS__
