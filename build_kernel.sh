ef2634d62b1c554b94ca95c69142f66894574cef
jimsterino98 <n.n.hoxha@gmail.com>
2026-06-21 21:57:34 -0700

    Refactor build_kernel.sh for simplified build process
    Remove KPM_BUILD configuration check and update make command.

--- build_kernel.sh
+++ build_kernel.sh
@@ -17,11 +17,4 @@ export ARCH=arm64
 export TARGET_SOC=s5e8835
 export BUILD_NUMBER=A546BXXUCDYDB
 make s5e8835-a54xnaxx_defconfig
-
-#if [ "$KPM" = "1" ]; then
-#    scripts/config --enable KPM_BUILD
-#else
-#    scripts/config --disable KPM_BUILD
-#fi
-
-make
+make -j$(nproc) Image
