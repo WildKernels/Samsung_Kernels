03a57a68127c030f45ca415560dfe5e4f96df0bc
jimsterino98 <n.n.hoxha@gmail.com>
2026-06-28 14:48:16 -0700

    Update build script for kernel compilation

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,8 +2,10 @@
 
 export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r450784d/bin:$PATH
 export PATH=$(pwd)/toolchain/build/kernel/build-tools/path/linux-x86/:$PATH
-export HOSTCFLAGS="--sysroot=$(pwd)/toolchain/build/kernel/build-tools/sysroot -I$(pwd)/toolchain/prebuilts/kernel-build-tools/linux-x86/include"
-export HOSTLDFLAGS="--sysroot=$(pwd)/toolchain/build/kernel/build-tools/sysroot  -Wl,-rpath,$(pwd)/toolchain/prebuilts/kernel-build-tools/linux-x86/lib64 -L $(pwd)/toolchain/prebuilts/kernel-build-tools/linux-x86/lib64 -fuse-ld=lld --rtlib=compiler-rt"
+export HOSTCFLAGS="-I$(pwd)/toolchain/prebuilts/kernel-build-tools/linux-x86/include -isystem /usr/include"
+export HOSTLDFLAGS="-L $(pwd)/toolchain/prebuilts/kernel-build-tools/linux-x86/lib64 \
+    -Wl,-rpath,$(pwd)/toolchain/prebuilts/kernel-build-tools/linux-x86/lib64 \
+    -fuse-ld=lld --rtlib=compiler-rt"
 
 export DTC_FLAGS="-@"
 export PLATFORM_VERSION=13
@@ -13,4 +15,4 @@ export DEPMOD=depmod
 export ARCH=arm64
 export TARGET_SOC=s5e8835
 make s5e8835-a54xnaxx_defconfig
-make
+make -j$(nproc) Image
