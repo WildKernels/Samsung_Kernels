abcb3087f76a5fd6f140df86a927f596bed4eecc
jimsterino98 <n.n.hoxha@gmail.com>
2026-08-07 22:42:41 -0700

    Clean up includes in base.c

--- kernel_platform/common/fs/proc/base.c
+++ kernel_platform/common/fs/proc/base.c
@@ -84,6 +84,8 @@
 #include <linux/nsproxy.h>
 #include <linux/oom.h>
 #include <linux/elf.h>
+#include <linux/task_integrity.h>
+#include <linux/proca.h>
 #include <linux/pid_namespace.h>
 #include <linux/user_namespace.h>
 #include <linux/fs_parser.h>
@@ -99,9 +101,6 @@
 #include <linux/resctrl.h>
 #include <linux/cn_proc.h>
 #include <linux/cpufreq_times.h>
-#include <linux/cn_proc.h>
-#include <linux/task_integrity.h>
-#include <linux/proca.h>
 #include <trace/events/oom.h>
 #include "internal.h"
 #include "fd.h"
