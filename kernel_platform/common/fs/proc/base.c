dd41da134cf143d6421e738c6718898ac862e051
jimsterino98 <n.n.hoxha@gmail.com>
2026-08-16 15:41:07 -0700

    refractor includes for susfs patching

--- kernel_platform/common/fs/proc/base.c
+++ kernel_platform/common/fs/proc/base.c
@@ -76,6 +76,8 @@
 #include <linux/security.h>
 #include <linux/ptrace.h>
 #include <linux/tracehook.h>
+#include <linux/task_integrity.h>
+#include <linux/proca.h>
 #include <linux/printk.h>
 #include <linux/cache.h>
 #include <linux/cgroup.h>
@@ -100,9 +102,6 @@
 #include <linux/resctrl.h>
 #include <linux/cn_proc.h>
 #include <linux/cpufreq_times.h>
-#include <linux/cn_proc.h>
-#include <linux/task_integrity.h>
-#include <linux/proca.h>
 #include <trace/events/oom.h>
 #include "internal.h"
 #include "fd.h"
