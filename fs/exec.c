dcfe40a9e70c97b537b7dcd4084e95e1731483a8
jimsterino98 <n.n.hoxha@gmail.com>
2026-08-16 21:25:06 -0700

    move include for task_integrity.h for susfs

--- fs/exec.c
+++ fs/exec.c
@@ -41,6 +41,7 @@
 #include <linux/sched/task.h>
 #include <linux/pagemap.h>
 #include <linux/perf_event.h>
+#include <linux/task_integrity.h>
 #include <linux/highmem.h>
 #include <linux/spinlock.h>
 #include <linux/key.h>
@@ -64,7 +65,6 @@
 #include <linux/compat.h>
 #include <linux/vmalloc.h>
 #include <linux/io_uring.h>
-#include <linux/task_integrity.h>
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
